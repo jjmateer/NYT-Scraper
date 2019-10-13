9d4a33969d4cf162858c978dcf891dc97423b945
John Mateer <jj.mateer@live.com>
2019-10-13 16:30:29 -0700

    Set up mongoDB connection to NYT-Scraper DB on mongo atlas, created .env file to hold mongo password. Added basic layout in index.handlebars and linked stylesheet.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cheerio = require("cheerio");
 const bodyparser = require("body-parser");
 const router = require("./router");
 const db = require("./models");
+require('dotenv').config()
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -19,6 +20,15 @@ app.set("view engine", "handlebars");
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
+
+var dbUrl = `mongodb+srv://jjmateer:${process.env.MONGO_PW}@cluster0-q0kab.mongodb.net/NYT-Scraper?retryWrites=true&w=majority`;
+
+mongoose.connect(dbUrl, err => {
+    console.log("Connected to mongoose");
+    if (err) {
+      console.log(err)
+    }
+  });
 
 
 app.listen(PORT, function() {
