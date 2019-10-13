32219155897ae90752eadff11ba44f785532de9a
John Mateer <jj.mateer@live.com>
2019-10-13 16:34:21 -0700

    Set up routing and exported controller functions.

--- controller/controller.js
+++ controller/controller.js
@@ -2,3 +2,15 @@
 exports.home = function (req, res) {
     res.render("index")
 }
+exports.scrape = function (req, res) {
+
+}
+exports.getAll = function (req, res) {
+
+}
+exports.getOne = function (req, res) {
+
+}
+exports.postNote = function (req, res) {
+
+}
--- router.js
+++ router.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const controller = require("./controller/controller.js")
 
 router.get("/", controller.home);
+router.get("/scrape", controller.scrape);
+router.get("/articles", controller.getAll);
+router.get("/articles/:id", controller.getOne);
+router.post("/articles/:id", controller.postNote);
+
 
 module.exports = router;
