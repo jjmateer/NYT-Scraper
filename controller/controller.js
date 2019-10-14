53e21ce9146b35c3dd079c0084fba1087f6c8dc6
John Mateer <jj.mateer@live.com>
2019-10-13 17:00:35 -0700

    Set up scrape function in controller to pull titles from nyt world news section of their website.

--- controller/controller.js
+++ controller/controller.js
@@ -1,9 +1,32 @@
-
+const axios = require("axios");
+const cheerio = require("cheerio");
+const db = require("../models");
 exports.home = function (req, res) {
     res.render("index")
 }
 exports.scrape = function (req, res) {
-
+    axios.get("https://www.nytimes.com/section/world").then(function (response) {
+        var $ = cheerio.load(response.data);
+        $("div h2").each(function (i, element) {
+            var result = {};
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+            result.summary = $(this)
+                .children("a")
+                .attr("href");
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+    })
+});
 }
 exports.getAll = function (req, res) {
 
