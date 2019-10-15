82a49cfdabc6c115ef65c4e4d3bc4d5abd20f690
John Mateer <jj.mateer@live.com>
2019-10-15 16:10:55 -0700

    css changes to note archive form.

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,8 @@ $("[name='modalBtn']").on("click", () => {
     $('#noteModal').modal("show")
 })
 
-$("#noteArchiveForm").on("submit", function() {
+$("#noteArchiveForm").on("submit", function(req, res) {
     event.preventDefault()  
     window.location.href += `/${$("#archiveID").val().trim()}`;
+    // res.redirect(`window.location.href/${$("#archiveID").val().trim()}`)
 })
